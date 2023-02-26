4e6ebcc5e1363706c7308e5b6e2b4624c80f0f24
paramont <ricardo.paramont.hg@gmail.com>
2023-02-26 17:27:07 -0600

    added lowest common ancestor of a binary tree.

--- Grind75/LowestCommonAncestorOfABinarySearchTree.cs
+++ Grind75/LowestCommonAncestorOfABinarySearchTree.cs
@@ -9,6 +9,24 @@ namespace Grind75
 {
     internal class LowestCommonAncestorOfABinarySearchTree
     {
-        
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            int maxValue = (p.val > q.val) ? p.val : q.val;
+            int minValue = (p.val > q.val) ? q.val : p.val;
+            TreeNode currentNode = root;
+
+            do
+            {
+                if (currentNode.val > maxValue)
+                    currentNode = currentNode.right;
+                else if (currentNode.val < minValue)
+                    currentNode = currentNode.left;
+                else
+                    return currentNode;
+            }while (currentNode != null);
+
+            return null;
+
+        }
     }
 }
