564a04aaa5e3689be2c370ba392766ca180f7e6c
paramont <ricardo.paramont.hg@gmail.com>
2023-03-03 14:10:50 -0600

    added AddBinary and MajorityElement.

---
+++ Grind75/Week2/AddBinary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind75.Week2
+{
+    class AddBinaryClass
+    {
+        public string AddBinary(string a, string b)
+        {
+            var result = new List<int>();
+
+            int aIndex = a.Length - 1;
+            int bIndex = b.Length - 1;
+            int carry = 0;
+
+            while (aIndex >= 0 || bIndex >= 0)
+            {
+                var sum = carry;
+
+                if (aIndex >= 0)
+                    sum += a[aIndex] - '0';
+
+                if (bIndex >= 0)
+                    sum += b[bIndex] - '0';
+
+                carry = sum / 2;
+                sum = sum % 2;
+
+                aIndex--;
+                bIndex--;
+                result.Add(sum);
+            }
+            if (carry > 0) result.Add(carry);
+
+            result.Reverse();
+            return string.Join("", result);
+        }
+    }
+}
---
+++ Grind75/Week2/MajorityElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind75.Week2
+{
+    class MajorityElementClass
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> elementCount = nums
+                .GroupBy(x => x)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            var halfLength = nums.Length / 2;
+            return elementCount.First(kvp => kvp.Value > halfLength).Key;
+        }
+    }
+}
