e109280f8655deb03e9ae44595ce70e766a6549a
paramont <ricardo.paramont.hg@gmail.com>
2023-03-15 15:53:18 -0600

    added last 2 problems of week 4

---
+++ Grind75/Week4/NumberOfIslands.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind75.Week4
+{
+    internal class NumberOfIslands
+    {
+        public int NumIslands(char[][] grid)
+        {
+            var visitedLand = new HashSet<(int, int)>(); // all positions with 1's
+            int nIslands = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == '1' && !visitedLand.Contains((i, j)))
+                    {
+                        ParseIslandsBFS(grid, i, j, visitedLand);
+                        nIslands++;
+                    }
+                }
+            }
+
+            return nIslands;
+        }
+
+
+        private void ParseIslandsBFS(char[][] grid, int initX, int initY, HashSet<(int, int)> visitedLand)
+        {
+
+            var landToParse = new Queue<(int, int)>(new[] { (initX, initY) });
+
+            while (landToParse.Count > 0)
+            {
+                (int x, int y) = landToParse.Dequeue();
+                visitedLand.Add((x, y));
+
+                // add valid neighbours
+                if (ValidCell(grid, x - 1, y, visitedLand))
+                    landToParse.Enqueue((x - 1, y));
+
+                if (ValidCell(grid, x + 1, y, visitedLand))
+                    landToParse.Enqueue((x + 1, y));
+
+                if (ValidCell(grid, x, y - 1, visitedLand))
+                    landToParse.Enqueue((x, y - 1));
+
+                if (ValidCell(grid, x, y + 1, visitedLand))
+                    landToParse.Enqueue((x, y + 1));
+            }
+
+        }
+
+        private bool ValidCell(char[][] grid, int x, int y, HashSet<(int, int)> visitedLand)
+        {
+            if (x >= 0 && x < grid.Length && y >= 0 && y < grid[0].Length)
+            {
+                if (grid[x][y] == '1' && !visitedLand.Contains((x, y)))
+                    return true;
+            }
+            return false;
+        }
+
+
+        // First Approach: combine hashset of all land with FLoodFill problem
+        // to expensive memory wise.
+        public int NumIslands1(char[][] grid)
+        {
+            int nRows = grid.Length;
+            int nCols = grid[0].Length;
+            var existingLand = new HashSet<(int, int)>(); // all positions with 1's
+
+            for (int i = 0; i < nRows; i++)
+            {
+                for (int j = 0; j < nCols; j++)
+                {
+                    if (grid[i][j] == '1')
+                        existingLand.Add((i, j));
+                }
+            }
+
+            int nIslands = 0;
+            while (existingLand.Count > 0)
+            {
+                (int x, int y) = existingLand.First();
+                ParseIslands1(grid, x, y, existingLand);
+                nIslands++;
+            }
+
+            return nIslands;
+        }
+
+
+
+
+        private void ParseIslands1(char[][] grid, int initX, int initY, HashSet<(int, int)> allLand)
+        {
+
+            var pixelsToProcess = new Queue<(int, int)>(new[] { (initX, initY) });
+
+            while (pixelsToProcess.Count > 0)
+            {
+                (int x, int y) = pixelsToProcess.Dequeue();
+
+                // add valid neighbours
+                if (allLand.Contains((x - 1, y)))
+                    pixelsToProcess.Enqueue((x - 1, y));
+
+                if (allLand.Contains((x + 1, y)))
+                    pixelsToProcess.Enqueue((x + 1, y));
+
+                if (allLand.Contains((x, y - 1)))
+                    pixelsToProcess.Enqueue((x, y - 1));
+
+                if (allLand.Contains((x, y + 1)))
+                    pixelsToProcess.Enqueue((x, y + 1));
+
+                allLand.Remove((x, y));
+            }
+
+        }
+
+    }
+}
---
+++ Grind75/Week4/RottingOranges.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind75.Week4
+{
+    internal class RottingOranges
+    {
+        public int OrangesRotting(int[][] grid)
+        {
+            var rotQueue = new Queue<(int, int)>();
+            var freshSet = new HashSet<(int, int)>();
+            // get the initially rotten oranges, plus set of fresh ones
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 2)
+                        rotQueue.Enqueue((i, j));
+                    else if (grid[i][j] == 1)
+                        freshSet.Add((i, j));
+                }
+            }
+
+            int time = 2;
+            var directions = new (int, int)[] { (-1, 0), (1, 0), (0, -1), (0, 1) };
+
+            // using BFS, rot subsequent oranges, also adding the time it took them to rot
+            while (rotQueue.Count > 0)
+            {
+                (int x, int y) = rotQueue.Dequeue();
+
+                foreach ((int xD, int yD) in directions)
+                {
+                    int newX = x + xD;
+                    int newY = y + yD;
+                    if (newX >= 0 && newX < grid.Length && newY >= 0 && newY < grid[0].Length)
+                    {
+                        if (grid[newX][newY] == 1)
+                        {
+                            freshSet.Remove((newX, newY));
+                            grid[newX][newY] = grid[x][y] + 1;
+                            time = Math.Max(time, grid[newX][newY]);
+                            rotQueue.Enqueue((newX, newY));
+                        }
+                    }
+                }
+            }
+
+            /*
+            for (int i = 0; i < grid.Length; i++)
+            {
+                Console.WriteLine(string.Join(",", grid[i]));
+            }
+            */
+            if (freshSet.Count > 0) return -1;
+
+            return time - 2;
+        }
+    }
+}
